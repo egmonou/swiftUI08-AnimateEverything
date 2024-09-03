c1487c34e7b92733667b4083fd3fb9896da732e7
Nour <mohamednour25@gmail.com>
2024-09-03 20:36:19 +0300

    Initial Commit

---
+++ AnimateEverything/AnimateEverythingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimateEverythingApp.swift
+//  AnimateEverything
+//
+//  Created by administrator on 03/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimateEverythingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
